2adb089b5311e35fbfc775ac5bd107a8448c7064
sumitkumar9 <sumit.ank0@gmail.com>
2023-08-21 18:01:33 +0530

    user signUp and signIn added

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -5,7 +5,7 @@ async function createGraphQlServer() {
   const gqlServer = new ApolloServer({
     typeDefs: `
             type Query {
-                hello: String
+              ${User.queries}
             }
             type Mutation {
                 ${User.mutations}
---
+++ src/services/user.ts
@@ -0,0 +1,55 @@
+import { createHmac, randomBytes } from "node:crypto";
+import Jwt from "jsonwebtoken";
+import { prismaClient } from "../lib/db";
+
+export interface CreateUserInterface {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+export interface GetUserTokenPayload {
+    email: string;
+    password: string;
+}
+
+const JWT_SECRET = "hiithisatest";
+
+export class UserService {
+
+    static generateHash(salt: string, password:string) {
+        const hashedPassword = createHmac("sha256", salt).update(password).digest("hex");
+        return hashedPassword;
+    }
+
+    static getUserByEmail(email: string) {
+        return prismaClient.user.findUnique({ where: {email}});
+    }
+
+    static createUser(payload: CreateUserInterface) {
+        const { firstName, lastName, email, password } = payload;
+        const salt = randomBytes(32).toString("hex");
+        const hashedPassword = UserService.generateHash(salt, password);
+        return prismaClient.user.create({data: {
+            firstName,
+            lastName, 
+            email,
+            password: hashedPassword,
+            salt,
+        }})
+    }
+
+    static async getUserToken(payload: GetUserTokenPayload) {
+        const { email, password } = payload;
+        const user = await UserService.getUserByEmail(email);
+        if (!user) throw new Error("user not found");
+        
+        const userHashPassword = UserService.generateHash(user.salt, password); 
+
+        if (userHashPassword !== user.password) throw new Error("incorrect passowrd");
+        const token = Jwt.sign({email: user.email, id: user.email}, JWT_SECRET);
+        return token;
+
+    }
+}
