f8e5ed6c9e678c14b1b7858440ace7bcce8c4edc
sumitkumar9 <60257266+sumitkumar9@users.noreply.github.com>
2023-08-21 18:26:49 +0530

    user signUp and signIn added (#4)

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -1,8 +1,15 @@
-const queries = {};
+import { CreateUserInterface, UserService } from '../../services/user';
+const queries = {
+    getUserToken: async (_:any, payload: {email: string, password:string}) => {
+        const token = await UserService.getUserToken({email: payload.email, password: payload.password})
+        return token;
+    }
+};
 
 const mutations = {
-    createUser: async (_: any,{}:{}) => {
-        return "ramdomId";
+    createUser: async (_: any, payload:CreateUserInterface) => {
+        const res = await UserService.createUser(payload);
+        return res.id;
     } 
 };
 
